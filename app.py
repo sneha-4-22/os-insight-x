39de690f09d1ea09d846f43ddaffb8587cca2286
sneha kumari <snehakumari64445@gmail.com>
2024-05-14 20:56:37 +0530

    👻

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
-from langchain.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st 
 
-
+st.set_page_config(
+    page_title="📚 Talk to Galvin's OS Textbook",
+    page_icon="👻",
+)
 load_dotenv()
 
-# Set Hugging Face Hub API token
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-# Define prompt template
 prompt_template = """To provide the best response, consider the following context and question carefully:
 
 Context: {context}
@@ -26,38 +25,35 @@ Provide an accurate and concise response based on the given context and question
 
 prompt = PromptTemplate(template=prompt_template, input_variables=["question", "context"])
 
-# Initialize Sentence Transformer embeddings
 embeddings = SentenceTransformerEmbeddings(model_name="llmware/industry-bert-insurance-v0.1")
 
-# Load documents
-loader = DirectoryLoader('data/', glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
-documents = loader.load()
+load_vector_store = Chroma(persist_directory="operatingsystem/embed", embedding_function=embeddings)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=70)
-texts = text_splitter.split_documents(documents)
+lvs = load_vector_store.as_retriever(search_kwargs={"k":2})
 
-
-vector_store = Chroma.from_documents(texts, embeddings, collection_metadata={"hnsw:space": "cosine"}, persist_directory="stores/insurance_cosine")
-
-# Initialize HuggingFace Hub
 repo = "llmware/bling-sheared-llama-1.3b-0.1"
-hfllm = HuggingFaceHub(repo_id=repo, model_kwargs={"temperature": 0.3, "max_length": 500})
+
+hfllm = HuggingFaceHub(
+    repo_id=repo, model_kwargs={"temperature": 0.3, "max_length": 500}
+)
+
+kwargs_type = {"prompt": prompt}
 
 def quesans():
     qa = RetrievalQA.from_chain_type(
-        llm=hfllm,
-        chain_type="stuff",
-        retriever=vector_store.as_retriever(search_kwargs={"k": 2}),
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": prompt},
-        verbose=True
+    llm=hfllm,
+    chain_type="stuff",
+    retriever=lvs,
+    return_source_documents=True,
+    chain_type_kwargs=kwargs_type,
+    verbose=True
     )
     return qa
 
 qa = quesans()
 
-# Define main function
 def main():
+
     st.title("📚 Talk to Galvin's OS Textbook ")
 
     st.markdown(
